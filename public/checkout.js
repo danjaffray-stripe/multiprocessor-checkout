13bcb2d49a3aa3b454e8dcfce4b9f3fdb9289d67
Dan Jaffray <danjaffray@stripe.com>
2024-03-27 15:59:04 +0000

    Initial commit

---
+++ public/checkout.js
@@ -0,0 +1,202 @@
+
+const testPublishabletKey = "pk_test_51KqK28E16bvDBxZbHHkDwYbHaxcMWqBm03u5BcKTSiv0fMab2gxLCPrESpLRRvzjHV8oEwlsZsBrO71ImLF0JYmA00DWNfbxvf"
+
+const stripe = Stripe(testPublishabletKey, options = {
+  betas: ['elements_enable_deferred_intent_beta_1', 'deferred_intent_pe_optional_amount_beta_0']});
+
+let elements
+let paymentElement
+let response
+let jsonData
+
+initialize();
+
+document.querySelector("#payment-form")
+document.addEventListener("submit", handleSubmit);
+
+// ------- Initialize Stripe Elements -------
+async function initialize() {
+
+  let amount = 1000;
+
+  showAmount(amount);
+
+  elements = stripe.elements({
+    mode: 'setup',
+    //amount: amount,
+    setupFutureUsage: 'on_session',
+    paymentMethodCreation: 'manual',
+
+  });
+
+  const paymentElementOptions = {
+    layout: "accordion",
+    fields: {
+      billingDetails: "auto"
+    }
+  };
+
+  paymentElement = elements.create("payment", paymentElementOptions);
+  paymentElement.mount("#payment-element");
+
+  var emailInput = document.getElementById('email');
+ 
+  // Attach the event listener for the 'submit' event
+    emailInput.addEventListener('change', function() {
+      paymentElement.update({defaultValues: {billingDetails: {email: emailInput.value}}});
+  });
+
+  paymentElement.on('change', function(event) {
+    console.log(event);
+  });
+
+}
+
+// ------- Handle Payment Submission -------
+async function handleSubmit(e) {
+  
+  e.preventDefault();
+  setLoading(true);
+
+  const {error: submitError} = await elements.submit();
+
+  if (submitError) {
+    showMessage(submitError);
+    return;
+  }
+  //  ------- Create PaymentMethod from Payment Element ------- 
+  try {
+
+    var { confirmationToken } = await stripe.createConfirmationToken({elements}); 
+    console.log(`confirmationToken created: ${confirmationToken.id} `)
+
+    showMessage("confirmationToken created successfully}");
+
+  } catch (error) {
+    console.log(error.message);
+    showMessage(error.message);
+    setLoading(false);
+
+  }
+
+  // ------- Create SetupIntent to run 3DSecure ------- 
+  try {
+    
+    response = await fetch("/create-setup-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ confirmationToken }),
+    });
+
+    jsonData = await response.json();
+    console.log(`setupIntent created: ${jsonData.id} `)
+    showMessage("SetupIntent created successfully");
+
+  } catch (error) {
+    console.log(error.message); 
+    setLoading(false);
+    showMessage(error.message);
+
+  }
+
+  //  ------- Run 3DSecure on clientSide ------- 
+  // The payment_method was associated with the SetupIntent above, so we only need the client secret to run 3DSecure
+  try {
+    
+    //var { setupIntent}  = await stripe.confirmCardSetup(
+    //  jsonData.client_secret
+    //);
+
+    var { setupIntent, error}  = await stripe.confirmSetup({
+      clientSecret: jsonData.client_secret,
+      confirmParams: {
+        return_url: 'https://example.com',
+        confirmation_token: confirmationToken.id,
+      },
+      redirect: 'if_required',
+    });
+
+    if (error) {
+      console.log(error.message);
+      showMessage(error.message);
+      setLoading(false);
+
+    } else {
+      console.log(setupIntent.status);
+      showMessage(setupIntent.status);
+    }
+  
+
+  } catch (error) {
+    console.log(error.message);
+    setLoading(false);
+    showMessage(error.message);
+  }
+
+  //  ------- Send SetupIntent to Server ------- 
+  try {
+    
+    response = await fetch("/payments", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ setupIntent }),
+    });
+
+    jsonData = await response.json();
+    console.log(jsonData)
+
+
+  } catch (error) {
+    console.log(error.message);
+    setLoading(false);
+    showMessage(error.message);
+
+  }
+  showMessage("PaymentIntent created successfully");
+  setLoading(false);
+}
+
+// ------- UI helpers -------
+function showMessage(messageText) {
+  const messageContainer = document.querySelector("#payment-message");
+
+  messageContainer.classList.remove("hidden");
+  messageContainer.textContent = messageText;
+
+  setTimeout(function () {
+    messageContainer.classList.add("hidden");
+  }, 6000);
+}
+
+function showAmount(amount) {
+  const messageContainer = document.querySelector("#amount-element");
+
+  messageContainer.innerHTML = `£${amount / 100}`;
+
+}
+
+function showResult(messageText) {
+  const messageContainer = document.querySelector("#result-message");
+  messageContainer.classList.remove("hidden");
+
+  messageContainer.innerHTML = `<pre> ${messageText} </pre>`;
+
+  setTimeout(function () {
+    messageContainer.classList.add("hidden");
+  }, 6000);
+
+}
+
+// ------- Show a spinner on payment submission -------- 
+function setLoading(isLoading) {
+  if (isLoading) {
+    // Disable the button and show a spinner
+    document.querySelector("#submit").disabled = true;
+    document.querySelector("#spinner").classList.remove("hidden");
+    document.querySelector("#button-text").classList.add("hidden");
+  } else {
+    document.querySelector("#submit").disabled = false;
+    document.querySelector("#spinner").classList.add("hidden");
+    document.querySelector("#button-text").classList.remove("hidden");
+  }
+}
---
+++ server.js
@@ -0,0 +1,278 @@
+const express = require('express');
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static("public"));
+
+const testSecretKey = process.env.TEST_SECRET_KEY
+const CHECKOUT_TEST_SECRET_KEY = process.env.CHECKOUT_TWO_TEST_KEY
+
+
+const stripe = require('stripe')(testSecretKey);
+const querystring = require("querystring");  
+const { exit } = require('process');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+let port = 4242
+
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
+  
+const createForwardingRequest = async (three_d_secure_data) => {  
+
+  const { authentication_flow, version, electronic_commerce_indicator, cryptogram, transaction_id } = three_d_secure_data
+
+  console.time(['my-forwarding-request'])
+
+  // Forwarding Config ID for Checkout.com gateway
+  const FORWARDING_CONFIG_ID = "fwdcfg_acct_TESTCONFIG_checkout_payments";  
+
+  const bodyData = {  
+    config: FORWARDING_CONFIG_ID,  
+    payment_method: "pm_1OywS6E16bvDBxZbkGabVgug",  
+    url: "https://api.sandbox.checkout.com/payments",
+    "request[headers][0][name]": "Authorization",
+    "request[headers][0][value]": "Bearer sk_sbox_cc3xyrd2mp7yurkdt2rywylenud",
+    "request[headers][1][name]": "Content-Type",
+    "request[headers][1][value]": "application/json",
+   // "request[headers][2][name]": "User-Agent",
+   // "request[headers][2][value]": "PostmanRuntime/7.37.0",
+    "request[body]":JSON.stringify(  
+    {
+      "amount": 10000,
+      "currency": "USD",
+      "reference": "Visa-USD-Test",
+      "3ds": {
+        "eci": "05",
+        "cryptogram": "M6+990I6FLD8Y6rZz9d5QbfrMNY=",
+        "xid": "M6+990I6FLD8Y6rZz9d5QbfrMNY=",
+        "version": "2.1.0",
+        "exemption": "low_value",
+        "challenge_indicator": "no_preference",
+      },
+      "source": {
+        "type": "card",
+        "number": "",
+        "expiry_month": "",
+        "expiry_year": "",
+        "name": "",
+        "cvv": "",
+        "billing_address": {
+          "address_line1": "123 High St.",
+          "address_line2": "Flat 456",
+          "city": "London",
+          "state": "GB",
+          "zip": "SW1A 1AA",
+          "country": "GB"
+        }
+      },
+    }) 
+  };  
+  
+  try {  
+    const response = await fetch("https://api.stripe.com/v1/forwarding/requests", {  
+      method: "POST",  
+      headers: {  
+        "Content-Type": "application/x-www-form-urlencoded",  
+        Authorization: `Bearer ${testSecretKey}`
+      },  
+      body: querystring.stringify(bodyData),  
+    });  
+  
+    console.log(querystring.stringify(bodyData))
+
+    const jsonData = await response.json();  
+
+    console.log("Headers:")
+    console.log(jsonData.request.headers)
+    console.log(jsonData);  
+    console.timeEnd('my-forwarding-request')
+    return jsonData;
+    
+  } catch (error) {  
+    console.log("Forwarding Request Error");  
+    console.trace(error);  
+  }  
+};  
+
+app.post('/create-forwarding-request', async (req, res) => {
+    const data = req.body;
+
+    try {
+        const forwardingRequest = await createForwardingRequest(data.paymentMethod.id)
+        res.json(forwardingRequest)
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+app.post('/create-setup-intent', async (req, res) => {
+    const data = req.body;
+
+    try {
+        var order_number = getRandomInt(100000, 999999)
+        var order_id = `Order - ${order_number}`
+
+        const setupIntent = await stripe.setupIntents.create({   
+            
+            automatic_payment_methods:{
+              enabled: true,
+              allow_redirects: "never"
+            },
+            //payment_method_types:["card"],
+            //confirm:"true",
+            usage:"on_session",
+            description: order_id,
+            customer: "cus_PoEuENNn4JGWab",
+            single_use:{
+              amount: 2000,
+              currency: "eur"
+            },
+            //confirmation_token: data.confirmationToken.id,
+            payment_method_options: {
+              card: {
+                request_three_d_secure: "any", // any | automatic 
+                  verify_card_account:"never",
+                  /*gateway:{
+                    acquirer_bin:  "424242",
+                    merchant_id:  "MITPOTYGNFQBEVD",
+                    requestor_id: "V0000000001234"
+                }*/
+              }
+            }
+          });
+
+          res.json(setupIntent);
+        
+    } catch (error) {
+        console.log(`Create SetupIntent error:  ${error.message}`)
+        res.json({
+          error: error.message
+        });
+    }
+});
+
+app.post('/payments', async (req, res) => {
+  const data = req.body;
+
+  console.log(`data recieved from frontend: ${data.setupIntent.id}`)
+
+  const setupIntentId = data.setupIntent.id
+
+  try {
+    var {payment_method, latest_attempt } = await stripe.setupIntents.retrieve(
+      setupIntentId, {
+        expand: ['latest_attempt']
+      }
+    );
+
+    const {payment_method_details: {card: {three_d_secure}}} = latest_attempt;
+
+    console.log(latest_attempt.payment_method_details.card.three_d_secure)
+
+  } catch (error) {
+    console.log(`Retrieve SetupIntent error:  ${error.message}`)    
+    res.json({
+      error: error.message
+    });
+    exit()
+  }
+  
+  try {
+    const forwardingRequest = await createForwardingRequest(latest_attempt.payment_method_details.card.three_d_secure)
+    res.json(forwardingRequest)
+
+  } catch (error) {
+    console.log(`Forwarding error:  ${error.message}`)    
+    res.json({
+      error: error.message
+    });
+    exit()
+  }
+
+  /*
+  try {
+    var order_number = getRandomInt(100000, 999999)
+    var order_id = `Order - ${order_number}`
+
+    const paymentIntent = await stripe.paymentIntents.create({    
+      automatic_payment_methods:{
+        enabled: true,
+        allow_redirects: "never"
+
+      },
+      confirm:"true",
+      off_session: "true",
+      description: order_id,
+      amount: 2000,
+      currency: "eur",
+      customer: "cus_PoEuENNn4JGWab",
+      expand: ['latest_charge'],
+      payment_method: payment_method,
+      //error_on_requires_action: "true",
+      payment_method_options: {
+        card: {
+          three_d_secure: {
+            version: latest_attempt.payment_method_details.card.three_d_secure.version,
+            electronic_commerce_indicator:latest_attempt.payment_method_details.card.three_d_secure.electronic_commerce_indicator,
+            cryptogram: "M6+990I6FLD8Y6rZz9d5QbfrMNY=",
+            transaction_id: latest_attempt.payment_method_details.card.three_d_secure.transaction_id
+          },
+        }
+      }
+    });
+
+    console.log(paymentIntent.status)
+    res.json(paymentIntent);
+      
+  } catch (error) {
+      console.log(`PaymentIntent error:  ${error.message}`)
+      res.json({
+          error: error.message
+        });
+  }
+  */
+});
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+const createCustomer = async () => {
+    try {
+        const customer = await stripe.customers.create({
+            email: 'danjaffray@gmail.com'
+        });
+        console.log(customer.id)
+      }
+     catch (error) {
+        console.log(error.message)
+    }
+  }
+
+const createPaymentMethod = async (card) => {
+    try {
+        const paymentMethod = await stripe.paymentMethods.create({
+            type: 'card',
+            card: {
+              number: card.number,
+              exp_month: card.exp_month,
+              exp_year: card.exp_year,
+              cvc: card.cvc,
+            },
+          });
+        console.log(paymentMethod)
+        return paymentMethod
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
