00cf2b981f654954f6626f60c774ecf2bbe560d3
Dan Jaffray <danjaffray@stripe.com>
2024-03-28 15:50:45 +0000

    Added configStripe.js as an export

--- public/checkout.js
+++ public/checkout.js
@@ -1,18 +1,4 @@
-async function fetchStripeConfig() {
-  try {
-      const response = await fetch('/stripe-config');
-      const config = await response.json();
-    
-      if (config.publishableKey) {
-          var stripe = Stripe(config.publishableKey);
-          return stripe
-      } else {
-          console.error('Publishable key is missing.');
-      }
-  } catch (error) {
-      console.error('Failed to fetch Stripe configuration:', error);
-  }
-}
+import { fetchStripeConfig } from './configStripe.js';
 
 let elements
 var stripe
---
+++ public/configStripe.js
@@ -0,0 +1,16 @@
+export async function fetchStripeConfig() {
+    try {
+        const response = await fetch('/stripe-config');
+        const config = await response.json();
+      
+        if (config.publishableKey) {
+            var stripe = Stripe(config.publishableKey);
+            return stripe
+        } else {
+            console.error('Publishable key is missing.');
+        }
+    } catch (error) {
+        console.error('Failed to fetch Stripe configuration:', error);
+    }
+  }
+  
